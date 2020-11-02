a004df0272a4e321baa76073ffb8e4f8471f7c99
mikczynski <mikolaj.skrzypczynski@gmail.com>
2020-11-02 16:01:57 +0100

    Modified User model and User migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,31 @@ namespace App\Model;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    /**
+     *  Model uses SoftDeletes to not delete records from DB.
+     *  Also while deleting we switch entity's is_active attribute to false.
+     *  User must verify email
+     */
+    use HasFactory, Notifiable, HasApiTokens, MustVerifyEmail, SoftDeletes;
+
+    /**
+     * The table associated with User model
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * @var bool
+     */
+    public $incrementing = true;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +36,24 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        #credentials
+        'login',
         'email',
         'password',
+
+        #profile
+        'first_name',
+        'last_name',
+        'birth_date',
+        'description',
+
+        #localization
+        'longitude',
+        'latitude',
+
+        #statuses
+        'is_admin',
+        'is_active',
     ];
 
     /**
@@ -31,7 +64,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'api_token' //?
     ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'is_admin' => false,
+        'is_active' => true,
+    ];
+
 
     /**
      * The attributes that should be cast to native types.
@@ -41,4 +86,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /* One-to-Many Relationships */
+    public function events(){
+        return $this->hasMany('App\Models\Event','event_creator_id','user_id');
+    }
+
+    public function participants(){
+        return $this->hasMany('App\Models\EventParticipant','user_id','user_id');
+    }
+
+    public function havingFriends(){
+        return $this->hasMany('App\Models\UserFriend','friend_id','user_id');
+    }
+
+    public function beingFriends(){
+        return $this->hasMany('App\Models\UserFriend','user_id','user_id');
+}
 }
