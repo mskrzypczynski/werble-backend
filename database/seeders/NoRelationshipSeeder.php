40265c550d6c15b9d5e730003a4f47f9e5ef3a94
mikczynski <mikolaj.skrzypczynski@gmail.com>
2021-01-09 20:02:49 +0100

    Update NoRelationshipSeeder.php

--- database/seeders/NoRelationshipSeeder.php
+++ database/seeders/NoRelationshipSeeder.php
@@ -25,17 +25,17 @@ class NoRelationshipSeeder extends Seeder
         User::factory(10)->create();
 
         $eventTypeNames =[
-                'Gym',
-                'Running',
-                'Birthday',
-                'Party',
-                'Swimming',
-                'Concert',
-                'Festival',
-                'Biking',
-                'Board games',
-                'Walk',
-                ];
+            'Biking',
+            'Birthday',
+            'Board games',
+            'Concert',
+            'Festival',
+            'Gym',
+            'Party',
+            'Running',
+            'Swimming',
+            'Walk',
+        ];
 
 
         foreach($eventTypeNames as $eventType){
